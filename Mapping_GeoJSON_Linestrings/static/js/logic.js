140190b4acf9be3b8f6d76b6d31214cb6cdb351e
DarlenePenn <dyvonnep@gmail.com>
2022-02-21 01:00:16 -0500

    route map

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -36,22 +36,23 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/DarlenePenn/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/DarlenePenn/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json"
 
+// Create a style for the lines.
+let myStyle = {
+  color: "#ffffa1",
+  weight: 2
+}
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  onEachFeature: function(feature, layer) {
-    console.log(layer)
-    layer.bindPopup("<h2>" + feature.properties.faa + "</h2> <hr> <h2>" + feature.properties.name + "</h2>");
-    
-   }
-
-  
-
-}).addTo(map);
-
+  L.geoJSON(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h2> Airline: " + feature.properties.airline + "</h2> <hr> <h2> Destination: " + feature.properties.dst + "</h2>");
+    }
+  })  
+.addTo(map);
 });
